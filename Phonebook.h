dce5e1d4cc04849b839623a111eede76f33801de
Omar El-Hariry <48570458+OmarHariry@users.noreply.github.com>
2019-10-01 22:10:42 +0200

    Add files via upload

---
+++ Phonebook.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include<ctype.h>
+int count (char str[]);
+void Switch(int n);
+int valided(char a[]);
+int validedx(char a[]);
+int validedz(char a[]);
+void clean();
+void load(char str[]);
+void save(char str[]);
+void search();
+void sort();
+void Delete();
+
+void menu(char str[],int n);
+
+void Modify();
+void Add();
+
